9e07736a9f288637c725c969fc4150cf2136d8c7
william-mx <william.engel@mdynamix.de>
2023-10-31 23:54:23 +0100

    initial commit

---
+++ run_ros_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set the permissions the X server host
+# see http://wiki.ros.org/docker/Tutorials/GUI
+xhost +local:docker
+
+# run ros docker container
+sudo docker run -it --rm \
+--mount type=bind,source=/home/mxck/mxck_ws/mxck_base,target=/melodic_ws \
+--mount type=bind,source=/dev,target=/dev \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--privileged \
+--net=host \
+--entrypoint bash \
+--name mxck_base_melodic \
+mxck_base_melodic:latest
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
