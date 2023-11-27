4951fffa309dea8abdfc9f69858c206835696aab
William Engel <william.engel@mdynamix.de>
2023-11-27 22:20:55 +0100

    add launch server to autorun

--- autorun.sh
+++ autorun.sh
@@ -26,7 +26,7 @@ source $MXCK_SH
 # run command
 if [ "$1" = "true" ];
 then
-  roslaunch mxck_run run_server:=true run_foxglove:=true
+  roslaunch mxck_run mxck_run.launch run_server:=true run_foxglove:=true
 else
   bash
 fi;
