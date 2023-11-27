429e0258d772f47e2186e4c4d46bb7489df2229f
William Engel <william.engel@mdynamix.de>
2023-11-27 21:23:47 +0100

    cutstom object detection init commit

--- src/object_detection/scripts/run_ssd_mobilenet.py
+++ src/object_detection/scripts/run_ssd_mobilenet.py
@@ -1,129 +1,158 @@
 #!/usr/bin/env python3
 
-from jetson_inference import detectNet
-from jetson_utils import videoSource, videoOutput, cudaFromNumpy
-
-
-import rospy
-import cv2
-import os
-from collections import deque
-from sensor_msgs.msg import Image
-from std_msgs.msg import String, Float32MultiArray
 from cv_bridge import CvBridge, CvBridgeError
+
+import cv2
 import numpy as np
+import rospkg
+import rospy
+import os
 
-class DetectNet:          
+from jetson_inference import detectNet
+from jetson_utils import cudaFromNumpy
 
-  def __init__(self, DEBUG = False):
+from sensor_msgs.msg import Image
+from std_msgs.msg import ColorRGBA, Header
+from foxglove_msgs.msg import ImageMarkerArray
+from visualization_msgs.msg import ImageMarker
+from geometry_msgs.msg import Point
+
+import matplotlib.pyplot as plt
+
+import pyrealsense2 as rs
+
+def publish(im, detections):
+  global id2color, bboxs, pub_markers, pub_image, bridge
   
-    
-    self.id2label = {0: 'BACKGROUND',
-                1:  'cross_parking',
-                2:  'no_overtaking',
-                3:  'overtaking',
-                4:  'parallel_parking',
-                5:  'pit_in',
-                6:  'pit_out',
-                7:  'traffic_light_green',
-                8:  'traffic_light_off',
-                9:  'traffic_light_red',
-                10: 'traffic_light_red_yellow',
-                11: 'traffic_light_yellow'} 
-
-    self.num_classes = len(self.id2label) # incl. background
-    self.N = 3 # mean confidence for N images
-    self.mean_confidence = [0] * self.num_classes
-    self.DEBUG = DEBUG
-    
-    self.conf_threshold = 0.5
-    self.id2confidence = {i: deque([0]*self.N, maxlen=self.N) for i in self.id2label.keys()}
-  
-    # paths
-    ws_dir = "/noetic_ws"
-    #model_dir = ws_dir + "/jetson-inference/python/training/detection/ssd/models/custom_dataset"
-    #model_dir = ws_dir + "/jetson-inference/python/training/detection/ssd/models/finetuning"
-    model_dir = ws_dir + "/jetson-inference/python/training/detection/ssd/models/traffic_light"
-    model_onnx = model_dir + "/ssd-mobilenet.onnx"
-    labels_txt = model_dir + "/labels.txt"
-    
-    for f in [ws_dir, model_dir, model_onnx]:
-      if not os.path.exists(f):
-        raise FileNotFoundError("No such file or directory: %s" %f)
-    
-    self.bridge = CvBridge()
-    
-    #build model
-    self.net = detectNet("ssd-mobilenet-v2", model=model_onnx, labels=labels_txt, threshold=0.5, \
-    input_blob='input_0', output_cvg='scores', output_bbox='boxes')
-    
-    # subscribe to camera message
-    self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback)
-    
-    # publish class
-    self.pub = rospy.Publisher('/prediction', String, queue_size=1)
-    self.bbox_pub = rospy.Publisher('/boundingbox', Float32MultiArray, queue_size=1)
-
-    self.bbox_msg = Float32MultiArray()
-    
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-      cuda_img = cudaFromNumpy(cv_image)
-      #rospy.loginfo("Image recieved")
-      detections = self.net.Detect(cuda_img)
-      
-      confs = [0] * self.num_classes
-      
-      bbox_data = []
-
-      for d in detections:
-        id_ = d.ClassID
-        label = self.id2label[id_]
-        confidence = round(d.Confidence, 2)
-        start_point = (int(d.Left), int(d.Top))
-        end_point = (int(d.Right), int(d.Bottom))
-        cv_image = cv2.rectangle(cv_image, start_point, end_point, (255,0,0), 2)
-        confs[id_] = confidence
-
-        if (confidence>=0.5):
-          bbox_data.extend([id_, d.Left, d.Top, d.Right, d.Bottom])
-
-        if self.DEBUG:
-          org = (int(d.Left), int(sorted([0, d.Top - 10, cv_image.shape[0]])[1]))
-          cv_image = cv2.putText(cv_image, "%s: %d%%" % (label, confidence*1e2), org, cv2.FONT_ITALIC, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-      self.bbox_msg.data = bbox_data
-
-      self.bbox_pub.publish(self.bbox_msg)
-        
-      for i in range(self.num_classes):
-        self.id2confidence[i].append(confs[i])
-        self.mean_confidence[i] = np.mean(self.id2confidence[i])
-      
-      if np.max(self.mean_confidence) >= self.conf_threshold:
-        id_ = np.argmax(self.mean_confidence)
-        lbl = self.id2label[id_]
-        self.pub.publish(lbl)
-        rospy.loginfo("%s detected: %s" % (lbl, str(self.id2confidence[id_])))
-      
-      if self.DEBUG:
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(200) # ms
-    except CvBridgeError as e:
-      print(e)
-
-    
-    
-if __name__ == '__main__':
-  rospy.init_node('detectnet', anonymous=True)     
-
-  sim = DetectNet(DEBUG=False)          
-
   try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    image_message = bridge.cv2_to_imgmsg(im_gray, encoding="mono8") # "bgr8"
+  except CvBridgeError as e:
+    print(e)
+  
+  pub_image.publish(image_message)
+  
+  bboxs.markers = []
+  for d in detections:
     
-  cv2.destroyAllWindows()
+    conf = d.Confidence
+    id_ = d.ClassID
+    
+    points=[
+      Point(d.Left, d.Top, 0),
+      Point(d.Right, d.Top, 0),
+      Point(d.Right, d.Bottom, 0),
+      Point(d.Left, d.Bottom, 0),
+    ]
+    
+    RGBA = id2color[id_] + (1,)
+
+    bboxs.markers.append(
+      ImageMarker(
+        header=Header(stamp = rospy.Time.now()),
+        scale=1,
+        type=ImageMarker.POLYGON,
+        outline_color=ColorRGBA(*RGBA),
+        points=points,
+      )
+    )
+  
+  pub_markers.publish(bboxs)
+        
+if __name__ == '__main__':
+  
+  rospy.init_node('ssd_mobilenet', anonymous=True)     
+
+  # Configure depth and color streams
+  pipeline = rs.pipeline()
+  config = rs.config()
+
+  # Get device product line for setting a supporting resolution
+  pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+  pipeline_profile = config.resolve(pipeline_wrapper)
+  device = pipeline_profile.get_device()
+  device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+  # check for rgb sensor
+  found_rgb = False
+  for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+      found_rgb = True
+      break
+  if not found_rgb:
+    raise Exception("Realsense D435i RGB Camera Not found!")
+    
+  w, h, fps = 640, 480, 15
+  config.enable_stream(rs.stream.color, w, h, rs.format.bgr8, fps)
+          
+  # build model
+  r = rospkg.RosPack()
+  model_dir = r.get_path('object_detection') + '/models/vdi_adc'
+  model_onnx = model_dir + "/ssd-mobilenet.onnx"
+  labels_txt = model_dir + "/labels.txt"
+  
+  for f in [model_dir, model_onnx, labels_txt]:
+    if not os.path.exists(f):
+      raise FileNotFoundError("No such file or directory: %s" %f)
+  
+  labels = open(labels_txt, 'r').read().splitlines()
+  n_classes = len(labels)
+  
+  id2color = {
+    0:  (1.0, 1.0, 1.0),
+    1:  (0.0, 0.5, 1.0),
+    2:  (1.0, 0.0, 1.0), 
+    3:  (1.0, 0.0, 0.8), 
+    4:  (0.0, 0.5, 0.8), 
+    5:  (0.0, 1.0, 1.0),
+    6:  (0.0, 0.8, 1.0),
+    7:  (0.0, 1.0, 0.0),
+    8:  (0.75, 0.75, 0.75),
+    9:  (1.0, 0.0, 0.0),
+    10: (1.0, 0.65, 0.0),
+    11: (1.0, 1.0, 0.0)
+  }
+  
+  
+  id2label = {i: lbl for i, lbl in enumerate(labels)}
+  print(id2label)
+  
+  threshold = 0.5 # confidence
+
+  #build model
+  net = detectNet("ssd-mobilenet-v2", model=model_onnx, labels=labels_txt, threshold=threshold, \
+  input_blob='input_0', output_cvg='scores', output_bbox='boxes')
+  
+  # publish ros topics
+  pub_image = rospy.Publisher("/camera/color/image_raw",Image, queue_size=1)
+  pub_markers = rospy.Publisher( "/bboxs", ImageMarkerArray, queue_size=1)
+  bboxs = ImageMarkerArray()
+  bridge = CvBridge()
+
+  # Start streaming
+  pipeline.start(config)
+  
+  try:
+    while not rospy.is_shutdown():
+
+      # Wait for a coherent pair of frames: depth and color
+      frames = pipeline.wait_for_frames()
+      color_frame = frames.get_color_frame()
+      if not color_frame:
+        continue
+
+      # Convert images to numpy arrays
+      color_image = np.asanyarray(color_frame.get_data())
+
+      cuda_img = cudaFromNumpy(color_image)
+      detections = net.Detect(cuda_img)
+      
+      publish(color_image, detections)
+
+  finally:
+
+    # Stop streaming
+    pipeline.stop()
+        
+
 
