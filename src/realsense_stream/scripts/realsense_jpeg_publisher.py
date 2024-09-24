66ed61c57730c057de48502f9f2cdab70d0b331b
william-engel <william.engel@web.de>
2024-09-24 16:53:30 +0200

    add ros cameraInfo topic

--- src/realsense_stream/scripts/realsense_jpeg_publisher.py
+++ src/realsense_stream/scripts/realsense_jpeg_publisher.py
@@ -5,7 +5,36 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 from sensor_msgs.msg import CompressedImage
-from cv_bridge import CvBridge
+from sensor_msgs.msg import CameraInfo
+
+def createCameraInfoMessage(intrinsics):
+
+    # Populate the CameraInfo message
+    camera_info_msg = CameraInfo()
+    camera_info_msg.header.stamp = rospy.Time.now()
+    camera_info_msg.header.frame_id = "camera_link_1"
+
+    camera_info_msg.width = intrinsics.width
+    camera_info_msg.height = intrinsics.height
+
+    # Intrinsic matrix
+    camera_info_msg.K = [intrinsics.fx, 0, intrinsics.ppx,
+                            0, intrinsics.fy, intrinsics.ppy,
+                            0, 0, 1]
+
+    # Distortion parameters (assuming plumb-bob model)
+    camera_info_msg.distortion_model = "plumb_bob"
+    camera_info_msg.D = list(intrinsics.coeffs)
+
+    # Rectification and projection matrices
+    camera_info_msg.R = [1.0, 0.0, 0.0,
+                            0.0, 1.0, 0.0,
+                            0.0, 0.0, 1.0]
+    camera_info_msg.P = [intrinsics.fx, 0, intrinsics.ppx, 0,
+                            0, intrinsics.fy, intrinsics.ppy, 0,
+                            0, 0, 1, 0]
+    
+    return camera_info_msg
 
 def validate_config(width, height, fps):
     valid_sizes = [
@@ -47,8 +76,11 @@ def main():
     config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, fps)
     pipeline.start(config)
 
-    bridge = CvBridge()
     pub = rospy.Publisher('/camera/color/image_jpeg', CompressedImage, queue_size=1)
+
+    info_pub = rospy.Publisher('/camera/color/camera_info', CameraInfo, queue_size=1, latch=True)
+
+    published_cam_info = False
 
     try:
         while not rospy.is_shutdown():
@@ -57,6 +89,17 @@ def main():
             if not color_frame:
                 continue
             
+            # Get intrinsics of the color sensor
+            intrinsics = color_frame.get_profile().as_video_stream_profile().get_intrinsics()
+
+            # Convert to CameraInfo ROS Message
+            info_msg = createCameraInfoMessage(intrinsics)
+
+            # Publish CameraInfo once
+            if not published_cam_info:
+                info_pub.publish(info_msg)
+                published_cam_info = True
+
             # Get current timestamp
             timestamp = rospy.Time.now()
 
