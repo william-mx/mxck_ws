65fa686d6a06e391b16e05af28665da6156d6556
william-mx <william.engel@mdynamix.de>
2025-04-21 09:00:28 +0200

    change urdf path to share dir and add check to prevent running the node twice

--- src/mxck_run/launch/broadcast_tf_launch.py
+++ src/mxck_run/launch/broadcast_tf_launch.py
@@ -1,34 +1,49 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable
 from launch_ros.parameter_descriptions import ParameterValue
-import os
-from pathlib import Path
-from ament_index_python.packages import get_package_prefix
+from ament_index_python.packages import get_package_share_directory
+import subprocess
 
 def generate_launch_description():
 
-    # Path to the URDF file in the source directory
-    pkg_dir = get_package_prefix('mxck_run').replace('install', 'src') #  /mxck2_ws/install/mxck_run → /humble_ws/src/mxck_run
-    urdf_path = pkg_dir + '/urdf/mxcarkit.urdf'
+    ld = LaunchDescription()
 
-    
+    # Path to the URDF file in the source directory
+    urdf_path = get_package_share_directory('mxck_run') + '/urdf/mxcarkit.urdf' # /mxck2_ws/install/mxck_run/share/mxck_run
+
     with open(urdf_path, 'r') as urdf_file:
         robot_description_content = urdf_file.read()
 
     robot_description = ParameterValue(robot_description_content, value_type=str)
 
     # Define nodes
-    robot_state_publisher_node = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name = 'robot_state_publisher',
         parameters=[
             {'robot_description': robot_description},
             {'publish_frequency': 20.0},  # Adjust this value as needed
-            {'use_tf_static': True}  # Set to True to publish static transforms only once
+            {'use_tf_static': True}  # True: publish /tf_static once; False: publish /tf_static at publish_frequency.
         ]
     )
 
-    return LaunchDescription([
-        robot_state_publisher_node
-    ])
+    # ===== Launch missing ROS2 nodes =====
+
+    # Get list of currently running nodes
+    result = subprocess.run(['ros2', 'node', 'list'], stdout=subprocess.PIPE, text=True)
+    
+    # Get node names and remove the leading slash
+    running_nodes = [node.lstrip('/') for node in result.stdout.strip().split('\n')] if result.stdout else []
+
+    # Map node name strings to their executables
+    node_names = {
+        'robot_state_publisher': robot_state_publisher,
+    }
+
+    # Launch node if it's not already running
+    for name, node in node_names.items():
+        if not name in running_nodes:
+            ld.add_action(node)
+
+    return ld
