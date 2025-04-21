2815c76ba8bf6b7c7df30c92c20a86a5782b18ca
william-mx <william.engel@mdynamix.de>
2025-04-21 08:58:28 +0200

    add urdf directory

--- src/mxck_run/setup.py
+++ src/mxck_run/setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
